e29b62737cd66d329646c8e9d2e4168255cf9c16
nikita202 <nikita@ostaplyuk.com>
2019-03-31 15:19:25 +0300

    aaaa

--- main.js
+++ main.js
@@ -36,9 +36,7 @@ const api_key = 'e96e601f143ecfa02400c818244cf635';
                         if (error) {
                             console.log(error);
                         } else {
-                            console.log(result);
-                            let resultArray = JSON.parse(result);
-                            resultArray.forEach(result => {
+                            result.forEach(result => {
                                 console.log(result.name + " (" + result.description + ")");
                                 if (json.rates[result.name]) {
                                     console.log("    Rate: " + json.rates[result.name]);
