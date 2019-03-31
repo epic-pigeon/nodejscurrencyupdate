fd050a60bca65792d5a90eab6e587219604c2196
nikita202 <nikita@ostaplyuk.com>
2019-03-31 19:57:29 +0300

    aaaa

---
+++ setupdb.js
@@ -0,0 +1,36 @@
+let request = require('request');
+let mysql = require('mysql');
+let mysqlOptions = {
+    host    : "localhost",
+    user    : "checkchecker",
+    password: "JJWMdF6riGuHDoVr",
+    database: "checkchecker"
+};
+let connection = mysql.createConnection(mysqlOptions);
+
+request("https://openexchangerates.org/api/currencies.json", function (error, response, body) {
+    if (error) {
+        console.log(error);
+    } else if (response.statusCode != 200) {
+        console.log("Bad status code: " + response.statusCode);
+    } else {
+        let json = {};
+        try {
+            json = JSON.parse(body);
+        } catch (e) {
+            console.log(e);
+        } finally {
+            connection.connect(function(err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    for (let key in json) {
+                        if (json.hasOwnProperty(key)) {
+                            connection.query(`INSERT INTO currencies (\`name\`, description) VALUES ('${key}', '${json[key]}')`, (err) => {if (err) console.log(err)});
+                        }
+                    }
+                }
+            });
+        }
+    }
+});
